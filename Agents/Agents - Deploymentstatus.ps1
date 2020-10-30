799c56647f001174a524e7624e8944004faf0c01
Patrick Kaak <pkaak@outlook.com>
2020-10-30 12:11:18 +0100

    Update Agents - Deploymentstatus.ps1

--- Agents/Agents - Deploymentstatus.ps1
+++ Agents/Agents - Deploymentstatus.ps1
@@ -1,34 +1,53 @@
-﻿# Agent Deployment Status
+# Agent Deployment Status
 # for Ivanti Security Controls
 # version 2019-12
 #
 # Changelog:
 # Dec 2019 - First Version
+# 2020-11: update for use of encrypted passwords
 #
 # patrick.kaak@ivanti.com
 # @pkaak
 
 #User variables
 $username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = '^[ISeC Serviceaccount Password]' #ISeC Credential password
+$password = "$[Password]" #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
+$securePW = "$[SecurePW]"
 
 $DeploymentID = '$[DeploymentID]'
 $iidoutput = '$[Output in Identity Director format]'
 
 #System variables
-$EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+if ($securePW -eq '0') 
+{
+  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+}
+else 
+{
+  try 
+  {
+    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
+  }
+  catch 
+  {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host -Object $ErrorMessage
+    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
+    exit(403)
+  }
+}
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $SetSessionCredentials = $True #Can we use SessionCredentials?
 
 
 #######################################################################################################################################################
-## Get CredentialID
+## Get Deployment information
 
 $url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/agents/deployment/' + $DeploymentID
 
-#Connect to ISeC REST API
+#Speak to ISeC REST API
 try 
 {
   $result = Invoke-RestMethod -Method Get -Credential $cred -Uri $url -ContentType 'application/json' | ConvertTo-Json -Depth 99
@@ -37,14 +56,14 @@ catch
 {
   # Dig into the exception to get the Response details.
   # Note that value__ is not a typo.
-  Write-Host -Object 'Error (CredentialsID)'
+  Write-Host -Object 'Error (DeploymentStatus)'
   Write-Host 'StatusCode:' $_.Exception.Response.StatusCode.value__ 
   Write-Host 'StatusDescription:' $_.Exception.Response.StatusDescription
   Write-Host 'Error Message:' $_.ErrorDetails.Message
   exit (2)
 }
 
-#REST API was OK. Go on
+#REST API was OK. Go futher
 $result = ConvertFrom-Json -InputObject $result
 
 #Results
