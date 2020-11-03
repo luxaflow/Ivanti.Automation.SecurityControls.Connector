52f7fc42fb49a9b50b3aa81943c8101a16f0dc6d
Patrick Kaak <pkaak@outlook.com>
2020-11-03 14:18:47 +0100

    Update Deployment Templates - Get ID.ps1

--- Deployment Templates/Deployment Templates - Get ID.ps1
+++ Deployment Templates/Deployment Templates - Get ID.ps1
@@ -1,16 +1,18 @@
-﻿# Deployment Templates - Get ID
+# Deployment Templates - Get ID
 # for Ivanti Security Controls
-# version 2019-08
+# version 2020-11
 #
 # Changelog
 # Aug 2019 - Better use of API
+# 2020-11: update for use of encrypted passwords
 # 
 # patrick.kaak@ivanti.com
 # @pkaak
 
 #User variables
 $username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = '^[ISeC Serviceaccount Password]' #ISeC Credential password
+$password = "$[Password]" #ISeC Credential password
+$securePW = "$[SecurePW]"
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
@@ -18,14 +20,31 @@ $DeploymentTemplateName = "$[Deployment Template Name]"
 
 #System variables
 $Url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/patch/deploytemplates?name=' + $DeploymentTemplateName
-$EncryptPassword = ConvertTo-SecureString $password -AsPlainText -Force
+if ($securePW -eq '0') 
+{
+  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+}
+else 
+{
+  try 
+  {
+    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
+  }
+  catch 
+  {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host -Object $ErrorMessage
+    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
+    exit(403)
+  }
+}
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 
 #Request body
 
 
-#Connect to ISeC REST API
+#Speak to ISeC REST API
 try 
 {
   $result = Invoke-RestMethod -Method Get -Credential $cred -Uri $Url -ContentType 'application/json' | ConvertTo-Json -Depth 99
