05c18da82bd0962ed8dd6b667b9c3b45144cdd1f
Patrick Kaak <pkaak@outlook.com>
2020-11-03 09:42:24 +0100

    Update Credentials - Get Credentials ID.ps1

--- Credentials/Credentials - Get Credentials ID.ps1
+++ Credentials/Credentials - Get Credentials ID.ps1
@@ -1,24 +1,43 @@
-﻿# Credentials - Get ID
+# Credentials - Get ID
 # for Ivanti Security Controls
-# version 2019-12
+# version 2020-11
 #
 # Changelog
 # Aug 2019 - Better use of API, rewrite of getting results based on different answer API
 # Dec 2019 - Update to use SessionCredentials
+# 2020-11: update for use of encrypted passwords
 # 
 # patrick.kaak@ivanti.com
 # @pkaak
 
 #User variables
 $username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = '^[ISeC Serviceaccount Password]' #ISeC Credential password
+$password = "$[Password]" #ISeC Credential password
+$securePW = "$[SecurePW]"
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
 $CredentialsName = "$[Credentials name]"
 
 #System variables
-$EncryptPassword = ConvertTo-SecureString $password -AsPlainText -Force
+if ($securePW -eq '0') 
+{
+  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+}
+else 
+{
+  try 
+  {
+    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
+  }
+  catch 
+  {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host -Object $ErrorMessage
+    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
+    exit(403)
+  }
+}
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 $SetSessionCredentials = $True #Can we use SessionCredentials?
@@ -182,7 +201,7 @@ $url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/credentials?
 
 #Request body
 
-#Connect to ISeC REST API
+#Speak to ISeC REST API
 try 
 {
   $result = Invoke-RestMethod -Method Get -Credential $cred -Uri $url -ContentType 'application/json' | ConvertTo-Json -Depth 99
