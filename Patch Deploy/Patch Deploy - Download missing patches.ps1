fb0ce6ccdbf3164019ed102dc1811cfbb8840674
Patrick Kaak <pkaak@outlook.com>
2020-11-04 13:40:19 +0100

    Update Patch Deploy - Download missing patches.ps1

--- Patch Deploy/Patch Deploy - Download missing patches.ps1
+++ Patch Deploy/Patch Deploy - Download missing patches.ps1
@@ -1,13 +1,18 @@
-﻿# Patch Downloads
+# Patch Downloads
 # for Ivanti Security Controls
-# version 2019-01
+# version 2020-11
+#
+# Changelog:
+# 2019-01: First version
+# 2020-11: update for use of encrypted passwords
 #
 # patrick.kaak@ivanti.com
 # @pkaak
 
 #User variables
 $username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = '^[ISeC Serviceaccount Password]' #ISeC Credential password
+$password = "$[Password]" #ISeC Credential password
+$securePW = "$[SecurePW]"
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
@@ -15,10 +20,27 @@ $ISEC_ID = "$[PatchScanID]"
 
 #System variables
 $Url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/patch/downloads/scans/'+$ISEC_ID
-$EncryptPassword = ConvertTo-SecureString $password -AsPlainText -Force
+if ($securePW -eq '0') 
+{
+  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+}
+else 
+{
+  try 
+  {
+    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
+  }
+  catch 
+  {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host -Object $ErrorMessage
+    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
+    exit(403)
+  }
+}
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 
-#Connect to ISeC REST API
+#Speak to ISeC REST API
 try 
 {
   $result = Invoke-RestMethod -Method Post -Credential $cred -Uri $Url -ContentType 'application/json' | ConvertTo-Json -Depth 99
