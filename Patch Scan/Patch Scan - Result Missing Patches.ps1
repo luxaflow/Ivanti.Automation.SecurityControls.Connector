3f92ba0706d338ffd82b02d47923bac539349219
Patrick Kaak <pkaak@outlook.com>
2020-11-04 13:59:17 +0100

    Update Patch Scan - Result Missing Patches.ps1

--- Patch Scan/Patch Scan - Result Missing Patches.ps1
+++ Patch Scan/Patch Scan - Result Missing Patches.ps1
@@ -1,13 +1,18 @@
-﻿# Patch Scan - ResultDetailed
+# Patch Scan - ResultDetailed
 # for Ivanti Security Controls
-# version 2019-11
+# version 2020-11
+#
+# Changelog:
+# 2019-11: First version
+# 2020-11: update for use of encrypted passwords
 #
 # patrick.kaak@ivanti.com
 # @pkaak
 
 #User variables
 $username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = '^[ISeC Serviceaccount Password]' #ISeC Credential password
+$password = "$[Password]" #ISeC Credential password
+$securePW = "$[SecurePW]"
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
@@ -18,13 +23,30 @@ $IIDoutput = '0'
 ## Step 1: Get resultID
 #System variables
 $Url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/patch/scans/'+$ISEC_ID+'/machines'
-$EncryptPassword = ConvertTo-SecureString $password -AsPlainText -Force
+if ($securePW -eq '0') 
+{
+  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+}
+else 
+{
+  try 
+  {
+    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
+  }
+  catch 
+  {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host -Object $ErrorMessage
+    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
+    exit(403)
+  }
+}
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 
 #Request body
 
-#Connect to ISeC REST API
+#Speak to ISeC REST API
 try 
 {
   $result = Invoke-RestMethod -Method Get -Credential $cred -Uri $Url -ContentType 'application/json' | ConvertTo-Json -Depth 99
@@ -47,7 +69,7 @@ $ResultID = $result.value.id
 
 ## Step 2: Get ScanPatches
 $Url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/patch/scans/'+$ISEC_ID+'/machines/'+$ResultID+'/patches'
-#Connect to ISeC REST API
+#Speak to ISeC REST API
 try 
 {
   $result = Invoke-RestMethod -Method Get -Credential $cred -Uri $Url -ContentType 'application/json' | ConvertTo-Json -Depth 99
