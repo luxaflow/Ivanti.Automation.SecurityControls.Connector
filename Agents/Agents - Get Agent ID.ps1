d667173d3f3127763b667af2697113b787bfea24
Patrick Kaak <pkaak@outlook.com>
2020-11-12 14:03:17 +0100

    Update Agents - Get Agent ID.ps1

--- Agents/Agents - Get Agent ID.ps1
+++ Agents/Agents - Get Agent ID.ps1
@@ -1,6 +1,6 @@
 # Agents - Get ID
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog
 # 2019-12: First version
@@ -14,31 +14,13 @@ $AgentName = '$[AgentName]'
 $IIDoutput = '$[IIDOutput]'
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
 #System variables
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 
 
