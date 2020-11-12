1643f2c238899f5ad7d7fda66d8a00a7a5203d0d
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:09:27 +0100

    Update Credentials - List.ps1

--- Credentials/Credentials - List.ps1
+++ Credentials/Credentials - List.ps1
@@ -1,6 +1,6 @@
 # Credentials - List
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog
 # Aug 2019 - Update listlimit to 50 (was 10) Nobody should have more than 50 credentials.. right?
@@ -11,32 +11,14 @@
 # @pkaak
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 $IIDoutput = "$[Output type]" #IID Output or List output
 
 #System variables
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 $SetSessionCredentials = $True #Can we use SessionCredentials?
