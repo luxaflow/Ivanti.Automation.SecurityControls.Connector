2e05884e8dc4062492624ccea82b2bebbc266333
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:54:56 +0100

    Update Patch Deploy - Deployed patches report.ps1

--- Patch Deploy/Patch Deploy - Deployed patches report.ps1
+++ Patch Deploy/Patch Deploy - Deployed patches report.ps1
@@ -1,6 +1,6 @@
 # Patch Deployment Result Summary
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog:
 # 2019-01: first version
@@ -10,9 +10,8 @@
 # @pkaak
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
@@ -21,24 +20,7 @@ $IIDoutput = "$[Output format]"
 
 #System variables
 $Url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/patch/deployments/'+$ISEC_ID+'/machines'
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 
 #Speak to ISeC REST API
