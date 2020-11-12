9f8efddfd963881cd055b3222ebd838feb165868
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:35:44 +0100

    Update MachineGroups - Remove Endpoint.ps1

--- MachineGroups/Endpoint/MachineGroups - Remove Endpoint.ps1
+++ MachineGroups/Endpoint/MachineGroups - Remove Endpoint.ps1
@@ -1,6 +1,6 @@
 # MachineGroup - Remove Endpoint
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog:
 # Aug 2019 - Update script to make better use of API and work with more than 50 machines in a group.
@@ -10,9 +10,8 @@
 # @pkaak
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
@@ -22,24 +21,7 @@ $MachineGroupID = "$[MachineGroup ID]"
 
 
 #System variables
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $MachineID = ''
 $output = ''
