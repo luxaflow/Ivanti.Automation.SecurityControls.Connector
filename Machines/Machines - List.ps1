5af6987d5d77fe7061942794bbf30201e70fc0fe
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:52:00 +0100

    Update Machines - List.ps1

--- Machines/Machines - List.ps1
+++ Machines/Machines - List.ps1
@@ -1,6 +1,6 @@
 # Machines - List
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog: 
 # 2020-08: First version
@@ -10,11 +10,10 @@
 # @pkaak
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$servername = '^[ISeC Servername]' #ISeC console servername
-$serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
+$servername = "^[ISeC Servername]" #ISeC console servername
+$serverport = "^[ISeC REST API portnumber]" #ISeC REST API portnumber
 
 #System variables
 $start = 1 #start ID to get machinegroup list from
@@ -22,24 +21,7 @@ $finished = 0 #did we find the end of the list
 $MachineList = [System.Collections.ArrayList]@() # Create array
 $WhereAreWe = 0
 
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 
@@ -72,11 +54,11 @@ while ($finished -eq 0)
   for ($count = 0;$count -lt $result.count; $count++) 
   {
     $obj = New-Object -TypeName PSObject -Property @{
-      id            = $result.value[$count].id
-      MachineName   = $result.value[$count].name
-      Domain        = $result.value[$count].domain
-      IPaddress     = $result.value[$count].ipAddress
-      AssignedGroup = $result.value[$count].assignedGroup
+      id   = $result.value[$count].id
+      MachineName = $result.value[$count].name
+      Domain   = $result.value[$count].domain
+      IPaddress   = $result.value[$count].ipAddress
+      AssignedGroup   = $result.value[$count].assignedGroup
       ISeCconsole   = $result.value[$count].consoleName
     }    
     $null = $MachineList.Add($obj)
@@ -85,18 +67,18 @@ while ($finished -eq 0)
   $sorted = $result.value | Sort-Object  -Property id -Descending   #sort ID to get highest ID
   if ($WhereAreWe -ne 0) 
   {
-    $start = ($sorted[0].id) + 1
+    $start = ($sorted[0].id) + 1 
   } #change start to highest ID
   if ($WhereAreWe -lt 50) 
   {
-    $finished = 1
+    $finished = 1 
   }
 }
 
 #Results
 
-#normal output
-#id, MachineName, Domain, IPaddress, AssignedGroup, ISeC Console
-$MachineList |
-Sort-Object -Property name |
-Format-Table -AutoSize -Property id, MachineName, Domain, IPaddress, AssignedGroup, ISeCconsole
+  #normal output
+  #id, MachineName, Domain, IPaddress, AssignedGroup, ISeC Console
+  $MachineList |
+  Sort-Object -Property name |
+  Format-Table -AutoSize -Property id, MachineName, Domain, IPaddress, AssignedGroup, ISeCconsole
