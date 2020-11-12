0dff8bafd3f366a39db00971108f3ada23b50a98
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:12:57 +0100

    Update Credentials - Unshare with user.ps1

--- Credentials/Credentials - Unshare with user.ps1
+++ Credentials/Credentials - Unshare with user.ps1
@@ -1,6 +1,6 @@
 # Credentials - Unshare with user
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog
 # Aug 2020 - First version
@@ -14,32 +14,14 @@ $CredentialsName = "$[Credential Friendly Name]"
 $ShareWithUser = "$[Users]"
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
 #System variables
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 $ShareWithUserArray = $ShareWithUser.Split(',')
