5f9cb049a4299818e9abef57b47f7f26fc26f2b1
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:47:42 +0100

    Update MachineGroups - List.ps1

--- MachineGroups/MachineGroups - List.ps1
+++ MachineGroups/MachineGroups - List.ps1
@@ -1,6 +1,6 @@
 # MachineGroups - List
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog: 
 # aug: list all machinegroups (and not only 10)
@@ -10,9 +10,8 @@
 # @pkaak
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 $IIDoutput = "$[Outputtype]" #IID Output or List output
@@ -23,24 +22,7 @@ $finished = 0 #did we find the end of the list
 $MachineList = [System.Collections.ArrayList]@() # Create array
 $WhereAreWe = 0
 
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 
