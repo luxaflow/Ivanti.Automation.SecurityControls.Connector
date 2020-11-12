f47a7c2318536d825f9a7f9278dc17a20889dc43
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:41:28 +0100

    Update MachineGroups - Create.ps1

--- MachineGroups/MachineGroups - Create.ps1
+++ MachineGroups/MachineGroups - Create.ps1
@@ -1,6 +1,6 @@
 # Machinegroup - Create
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Dec 2019: Update to set Credentials when creating a MachineGroup
 # 2020-11: update for use of encrypted passwords
@@ -15,32 +15,14 @@ $BodyMachineGroupFolder = "$[Folder]"
 $BodyMachineGroupDescription = "$[Machinegroup Description]"
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 $CredentialsName = '$[Credentials Name]'
 
 #System variables
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $CredentialID = '' 
 
