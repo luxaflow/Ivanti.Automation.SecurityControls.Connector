57b0e7bca2698fc5661b0790ff970c5321308fc3
Patrick Kaak <pkaak@outlook.com>
2020-11-12 15:17:14 +0100

    Update Credentials - Update.ps1

--- Credentials/Credentials - Update.ps1
+++ Credentials/Credentials - Update.ps1
@@ -1,6 +1,6 @@
 # Credentials - Update
 # for Ivanti Security Controls
-# version 2020-11
+# version 2020-11.12
 #
 # Changelog
 # Aug 2019 - First edition
@@ -11,35 +11,17 @@
 # @pkaak
 
 #User variables
-$username = '^[ISeC Serviceaccount Username]' #ISeC Credential Username
-$password = "$[Password]" #ISeC Credential password
-$securePW = "$[SecurePW]"
+$username = Get-ResParam -Name Username #ISeC Credential Username
+$password = Get-ResParam -Name Password #ISeC Credential password
 $servername = '^[ISeC Servername]' #ISeC console servername
 $serverport = '^[ISeC REST API portnumber]' #ISeC REST API portnumber
 
 $CredentialsName = "$[Credential Friendly Name]"
-$CredentialsUserName = "$[Credential Username]"
-$credentialsPassword = "$[Credential Password]"
+$SecurePassword = Get-ResParam -Name "Credential Password"
+$CredentialsUsername = Get-ResParam -Name "Credential Username"
 
 #System variables
-if ($securePW -eq '0') 
-{
-  $EncryptPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-}
-else 
-{
-  try 
-  {
-    $EncryptPassword = ConvertTo-SecureString $password -ErrorAction Stop
-  }
-  catch 
-  {
-    $ErrorMessage = $_.Exception.Message
-    Write-Host -Object $ErrorMessage
-    Write-Host -Object 'Error 403: Did you run this task on the same machine which encrypted the password?'
-    exit(403)
-  }
-}
+$EncryptPassword = $password
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptPassword
 $output = ''
 $CredentialsID = '' #
@@ -243,6 +225,13 @@ if ($found -eq 0)
 ## Now update the credentials
 
 $url = 'https://'+$servername+':'+$serverport+'/st/console/api/v1.0/credentials/' + $CredentialsID
+
+# Secure to Unsecure 
+# I know, this will be changed in later versions to use the secure methode
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+$credentialsPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+[Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
+
 #Request body
 $body = @{
   name     = $CredentialsName
